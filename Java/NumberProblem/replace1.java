48dd1f57d5fc1404daf5a5d12cff34efcdc579ee
HarshitVarshneyv <harshitvarshneyv@gamil.com>
2025-08-30 21:41:37 +0530

    New change

--- Java/NumberProblem/replace1.java
+++ Java/NumberProblem/replace1.java
@@ -1,12 +1,18 @@
 package Java.NumberProblem;
 
 class replace1WithZero {
-    int n = 102003;
-    String r = "";
-    String str = String.valueOf(n); 
-    for (char ch : str.toCharArray()) {
-        if(ch == '0'){
-            r += '1'; 
-        }else r += ch;  
+
+    public static void main(String[] args) {
+        int n = 102003;
+        String r = "";
+        String str = String.valueOf(n);
+        for (char ch : str.toCharArray()) {
+            if (ch == '0') {
+                r += '1';
+            } else {
+                r += ch;
+            }
+        }
+        System.out.println(r);
     }
 }
