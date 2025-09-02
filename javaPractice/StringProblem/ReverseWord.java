7cc839f7f07003ca6073008a560975b6ed2d83e4
HarshitVarshneyv <harshitvarshneyv@gmail.com>
2025-09-02 17:24:52 +0530

    Bew commit

--- javaPractice/StringProblem/ReverseWord.java
+++ javaPractice/StringProblem/ReverseWord.java
@@ -1,8 +1,39 @@
 package javaPractice.StringProblem;
 
+import java.util.Scanner;
+
 public class ReverseWord {
     public static void main(String[] args) {
-        String  s ="this is an amazing program";
-        
+        // String str ="this is an amazing program";
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        effcetiveApproach(str);
+        String revStr = new StringBuilder(str).reverse().toString();
+        //System.out.println("Original string get reversed => " + "\n" + revStr);
+
+        String result = "" , word = "";
+        for(int i=0;i<revStr.length()-1;i++){
+                while(i < revStr.length() && revStr.charAt(i) != ' '){
+                    word += revStr.charAt(i);
+                    i++;
+                }
+            String revWord = new StringBuilder(word).reverse().toString();
+            result += " " + revWord;
+            word = "";
+        }
+        System.out.println(result.substring(1));
+    }
+
+    public static void effcetiveApproach(String str){
+        String[] words = str.split(" ");
+        String result = "";
+
+        for(int i = words.length - 1 ; i >= 0;i--){
+            result += words[i];
+            if(i != 0){
+                result += " ";
+            }
+        }
+        System.out.println(result);
     }
 }
