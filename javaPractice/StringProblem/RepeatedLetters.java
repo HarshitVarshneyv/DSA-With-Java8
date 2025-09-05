fc532eba7f12e4e5ca73048520a90c35c2e85d7c
HarshitVarshneyv <harshitvarshneyv@gmail.com>
2025-09-05 13:07:24 +0530

    new changes

--- javaPractice/StringProblem/RepeatedLetters.java
+++ javaPractice/StringProblem/RepeatedLetters.java
@@ -1,11 +1,34 @@
+/*
+Find word with highest number of repeated letters in string Example 1: 
+Input:string=”abcdefghij google microsoft” Output: google 
+Explanation: In “google” g appears 2 times, o appears 2 times which is highest among all words
+ */
+
 package javaPractice.StringProblem;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class RepeatedLetters {
     public static void main(String[] args) {
         String s = "abcdefghij google microsoft";
-        HashMap<Character,Integer> map = new HashMap<>();
+        String[] words = s.split(" ");
+        String resultWord = "";
+        int maxRepeatCount = 0;
+
+        for(String word : words){
+            HashMap<Character,Integer> map = new HashMap<>();
+            for(char ch : word.toCharArray()){
+                map.put(ch, map.getOrDefault(ch,0) + 1);
+            }
+            int currMax = 0;
+            for(int count : map.values()){
+                if(count > currMax) currMax = count;
+            }
+            if(currMax > maxRepeatCount){
+                resultWord = word;
+                maxRepeatCount = currMax;
+            }
+        }
+        System.out.println(resultWord);
     }
 }
