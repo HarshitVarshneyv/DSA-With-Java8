f08bf53acad582c2514395e3c66068b898992b2f
HarshitVarshneyv <harshitvarshneyv@gmail.com>
2025-09-06 21:21:23 +0530

    String completed

---
+++ javaPractice/StringProblem/Capital1andLast.java
@@ -0,0 +1,22 @@
+package javaPractice.StringProblem;
+
+import java.util.Arrays;
+
+public class Capital1andLast {
+    public static void main(String[] args) {
+        String str = "Take u Forward is Awesome";
+        String[] words = str.split(" ");
+
+        StringBuilder s = new StringBuilder();
+
+        for(String word : words ){
+            for(int i=0;i<word.length();i++){
+                if(i == 0 || i == word.length() - 1){
+                    s.append(Character.toUpperCase(word.charAt(i)));
+                } else s.append(word.charAt(i));
+            }
+            s.append(" ");
+        }
+        System.out.println(s.toString());
+    }
+}
