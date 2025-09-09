7846152c567f9a8ecd99b961914bba03545aa5db
HarshitVarshneyv <harshitvarshneyv@gmail.com>
2025-09-09 20:50:42 +0530

    New changes

--- javaPractice/LinkedList/Implem.java
+++ javaPractice/LinkedList/Implem.java
@@ -20,6 +20,46 @@ class LinkedList{
         size = 0;
     }
 
+    public void addAtHead(int val){
+        Node newNode = new Node(val);
+        size++;
+        if(head == null){ 
+            head = tail = newNode;
+            return;
+        } else{ // New noorka next pointer head ke Taraf point karega tabi connection build hoga.
+        newNode.next = head;
+        head = newNode;
+        }  
+    }
+
+    public void addAtTail(int val){
+        Node newNode = new Node(val);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void addAtIndex(int idx, int val){
+        if(idx < 0 || idx > size) return;
+        if(idx == 0) {
+            addAtHead(val);
+        } else if(idx == size){
+            addAtTail(val);
+        } else{
+            Node newNode = new Node(val);
+            size++;
+            Node temp = head;
+            for (int i = 0; i < idx - 1; i++) temp = temp.next;
+            newNode.next = temp.next;
+            temp.next = newNode;
+
+        }
+    }
 }
 class Main {
     
