0123866d82304f927a00ca4f59fa65e88dcc8812
HarshitVarshneyv <harshitvarshneyv@gmail.com>
2025-09-12 22:45:03 +0530

    Fully linked list implementation done

--- javaPractice/LinkedList/Implem.java
+++ javaPractice/LinkedList/Implem.java
@@ -26,7 +26,7 @@ class LinkedList{
         if(head == null){ 
             head = tail = newNode;
             return;
-        } else{ // New noorka next pointer head ke Taraf point karega tabi connection build hoga.
+        } else{ // New node ka next pointer head ke Taraf point karega tabi connection build hoga.
         newNode.next = head;
         head = newNode;
         }  
@@ -59,7 +59,28 @@ class LinkedList{
             }
             newNode.next = temp.next;
             temp.next = newNode;
-            
+        }
+    }
+
+    public void deleteAtIndex(int idx){
+        if(idx < 0 || idx >= size) return;  
+        if(idx == 0){
+            head = head.next;
+            size--;
+            if(size == 0) tail = null; // ✅ edge case
+        }
+        Node prev = head;
+        for(int i=0;i<idx-1;i++) prev = prev.next;
+        prev.next = prev.next.next;
+        if(idx == size-1) tail = prev;  // ✅ tail update
+        size--;
+    }
+
+    public void display(){
+        Node temp = head;
+        while(temp != null){
+            System.out.println(temp.val);
+            temp = temp.next;
         }
     }
 }
