966aba24a21d149a66fce7820454f9c871b1610a
HarshitVarshneyv <harshitvarshneyv@gmail.com>
2025-09-15 20:20:03 +0530

    New

--- javaPractice/realLinkedList/Reverse.java
+++ javaPractice/realLinkedList/Reverse.java
@@ -21,7 +21,7 @@ public class Reverse {
     public static void main(String[] args) {
         int[] arr = {1,2,3};
         for(int i=0;i<arr.length;i++){
-            
+            System.out.println("This is the loop");
         }
     }
 }
