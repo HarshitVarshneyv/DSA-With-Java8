b55888b4036c661d7ecdce5595f10360798ba292
HarshitVarshneyv <harshitvarshneyv@gmail.com>
2025-09-22 13:24:26 +0530

    Doing progress on the Linked list

--- javaPractice/realLinkedList/a_Reverse.java
+++ javaPractice/realLinkedList/a_Reverse.java
@@ -17,7 +17,7 @@ class Solution{
 
 }
 
-public class Reverse {
+class Reverse {
     public static void main(String[] args) {
         int[] arr = {1,2,3};
         for(int i=0;i<arr.length;i++){
---
+++ javaPractice/realLinkedList/b_middle.java
@@ -0,0 +1,7 @@
+package javaPractice.realLinkedList;
+
+public class b_middle {
+    public static void main(String[] args) {
+        
+    }
+}
