56edbb3bf0e2ae8216bd7225c1e6b512e597bc37
HarshitVarshneyv <harshitvarshneyv@gmail.com>
2025-10-10 00:41:04 +0530

    The new step problem has been solved

---
+++ javaPractice/Stack/MediumProblem/minOfMax.java
@@ -0,0 +1,34 @@
+package javaPractice.Stack.MediumProblem;
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 50, 10, 70, 30};
+        int[] result = maxOfMinBrute(arr);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] maxOfMinBrute(int[] arr) {
+        int n = arr.length;
+        int[] res = new int[n];
+
+        for (int k = 1; k <= n; k++) {
+            int maxMin = Integer.MIN_VALUE;
+
+            for (int i = 0; i <= n - k; i++) {
+                int minVal = Integer.MAX_VALUE;
+
+                for (int j = i; j < i + k; j++) {
+                    minVal = Math.min(minVal, arr[j]);
+                }
+
+                maxMin = Math.max(maxMin, minVal);
+            }
+
+            res[k - 1] = maxMin;
+        }
+
+        return res;
+    }
+}
