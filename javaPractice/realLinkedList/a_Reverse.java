d1b83121d2c557bec865b189e9dfeaf95bc0cd6d
Harshit Varshney <harshitvarshneyv@gmail.com>
2025-11-29 22:08:39 +0530

    Update a_Reverse.java

--- javaPractice/realLinkedList/a_Reverse.java
+++ javaPractice/realLinkedList/a_Reverse.java
@@ -1,27 +1,55 @@
-package javaPractice.realLinkedList;
-
+// Definition for singly-linked list
 class ListNode {
-    ListNode next;
     int val;
+    ListNode next;
 
     ListNode(int val) {
         this.val = val;
+        this.next = null;
     }
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-};
-
-class Solution{
-
 }
 
-class Reverse {
-    public static void main(String[] args) {
-        int[] arr = {1,2,3};
-        for(int i=0;i<arr.length;i++){
-            System.out.println("This is the loop");
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null, curr = head, next = null;
+
+        while (curr != null) {
+            next = curr.next;   // store next node
+            curr.next = prev;   // reverse pointer
+            prev = curr;        // move prev forward
+            curr = next;        // move curr forward
         }
+        return prev; // new head
+    }
+
+    // Helper method to print linked list
+    public void printList(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    // Main method for testing
+    public static void main(String[] args) {
+        // Create linked list: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        Solution sol = new Solution();
+
+        System.out.println("Original List:");
+        sol.printList(head);
+
+        // Reverse the list
+        ListNode reversed = sol.reverseList(head);
+
+        System.out.println("Reversed List:");
+        sol.printList(reversed);
     }
 }
